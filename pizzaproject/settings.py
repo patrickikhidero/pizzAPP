0674c767e1745d555519e1736ed6ceb127d492a2
patrickikhidero <80291797+patrickikhidero@users.noreply.github.com>
2022-02-07 19:31:17 +0100

    update-and-delete-method

--- pizzaproject/settings.py
+++ pizzaproject/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'phonenumber_field',
     'phonenumbers',
+    'corsheaders',
 
     # local
     'stores',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'pizzaproject.urls'
 
--- stores/serializers.py
+++ stores/serializers.py
@@ -1,8 +1,11 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Pizzeria
+from rest_framework.reverse import reverse
+
 
 class PizzeriaListSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Pizzeria
         fields = [
@@ -10,9 +13,16 @@ class PizzeriaListSerializer(serializers.ModelSerializer):
             'pizzeria_name',
             'city',
             'zip_code',
+            'absolute_url',
         ]
 
+    def get_absolute_url(self, obj):
+        return reverse('pizzeria_detail', args=(obj.pk,))
+
+
 class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    update = serializers.SerializerMethodField()
+    delete = serializers.SerializerMethodField()
     class Meta:
         model = Pizzeria
         fields = [
@@ -27,5 +37,12 @@ class PizzeriaDetailSerializer(serializers.ModelSerializer):
             'description',
             'logo_image',
             'email',
-            'active'
+            'active',
+            'update',
+            'delete',
         ]
+    def get_update(self, obj):
+        return reverse('pizzeria_update', args=(obj.pk,))
+
+    def get_delete(self, obj):
+        return reverse('pizzeria_delete', args=(obj.pk))
